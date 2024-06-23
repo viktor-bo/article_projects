5a289efa749e33b8df535036183a3285d6b71f51
viktor <viktor.baldji@gmail.com>
2024-06-23 14:40:10 +0600

    создал модель Article, зарегистрировал ее в админке

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False, verbose_name='Заголовок')
+    content = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Контент')
+    author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return f'{self.pk}. {self.title}: {self.author}'
+
+    class Meta:
+        db_table = "articles"
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
